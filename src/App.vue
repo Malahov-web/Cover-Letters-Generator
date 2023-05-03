afad9dafe16aea4a72b63ccda32bee0fe521ef4b
Kirill Malakhov <malahovk@gmail.com>
2023-05-03 20:26:39 +0300

    2 Add: Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
+``
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Cover Letters Generator</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container grid-list-xs>
+        <v-row :align="align" no-gutters __style="height: 150px">
+          <!-- <v-col lg="8" offset-lg="2"> -->
+          <!-- Search placeholder  -->
+          <!-- </v-col> -->
+          <!-- <v-col lg="4">
+            <ViewControls class="asd"></ViewControls>
+          </v-col> -->
+        </v-row>
+      </v-container>
+
+      <v-container grid-list-xs>
+        <v-col lg="12">
+          <!-- <ViewControls class="asd"></ViewControls> -->
+        </v-col>
+        <!-- <Day class="asd"></Day> -->
+        <!-- <HelloWorld class="asd"></HelloWorld> -->
+        <HomeView class="asd"></HomeView>
+        <!-- <hello-world /> -->
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld";
+import HomeView from "./views/HomeView";
 
 export default {
   name: "App",
+
   components: {
-    HelloWorld,
+    // HelloWorld,
+    HomeView,
   },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" __scoped>
+// @import url();
+// @import "../assets/styles/styles"; // main styles
+// src\assets\styles\styles.scss
+// src\assets\styles\style.scss
+
+// C:\HTML 2\Cover Letters Generator\Cover-Letters-Generator\src\assets\styles\styles.scss
+// @import "../assets/styles/styles"; // -
+// @import "../assets/styles/styles.scss"; // -
+// @import "../assets/styles/style"; // -
+// @import "/assets/styles/style"; // + конечно, это скопировано из проекта, где App был внутри какой-то папки
+@import "assets/styles/style"; // + также, только без слэша
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <!-- HomeView -->
+
+    <v-row>
+      <v-col lg="8" offset-lg="2" class="mt-12 mb-8">
+        <div class="site-title-outer __mt-6 __mb-6">
+          <div class="site-title text-center">Generate your Cover Letter</div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <v-row __no-gutters>
+      <v-col lg="8" offset-lg="2" class="preview-outer">
+        <div class="preview px-16 py-8">
+          <div class="preview-inner">
+            <div class="preview__content">
+              <!-- TODO: -->
+              <!-- Вероятно тэг per будет не нужен -->
+
+              <pre>
+Здравствуйте.
+Интересует работа в вашей компании (удаленно),
+на позиции Frontend-разработчика.
+
+Имею большой опыт в верстке html/css, (сайты, лендинги, интерфейсы, emails, spa), 
+из плюсов имею опыт работы с  Vue.js, GSAP, и практику оптимизации сайтов. 
+
+Ищу работу в удаленном формате и  команду в которой можно вырасти.
+Мое портфолио:
+http://malahov-web.com/portfolio/
+                </pre
+              >
+            </div>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+
+    <div class="generator"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+};
+</script>
+
+<style lang="scss" scoped></style>
